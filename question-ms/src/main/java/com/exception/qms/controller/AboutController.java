a5d11edb55eafd0dadf629544c290c9e99d95892
weiwosuoai <weiwosuoai1991@gmail.com>
2018-05-10 15:16:32 +0800

    添加 Dockerfile

--- question-ms/src/main/java/com/exception/qms/controller/AboutController.java
+++ question-ms/src/main/java/com/exception/qms/controller/AboutController.java
@@ -3,8 +3,11 @@ package com.exception.qms.controller;
 import com.exception.qms.aspect.OperatorLog;
 import com.exception.qms.business.QuestionBusiness;
 import com.exception.qms.common.BaseController;
+import com.exception.qms.enums.ResponseModelKeyEnum;
+import com.exception.qms.enums.TopNavEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,7 +27,8 @@ public class AboutController extends BaseController {
      */
     @GetMapping("/about")
     @OperatorLog(description = "关于页面")
-    public String showAboutPage() {
+    public String showAboutPage(Model model) {
+        model.addAttribute(ResponseModelKeyEnum.TOP_NAV.getCode(), TopNavEnum.ABOUT.getCode());
         return "about/about";
     }
 
