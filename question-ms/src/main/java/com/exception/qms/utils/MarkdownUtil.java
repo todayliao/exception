bf14c540efc25acce4f362fd1198c01c6fdf21ab
weiwosuoai <weiwosuoai1991@gmail.com>
2018-06-25 15:19:48 +0800

    修复后台 md 解析器解析 table 错误，以及 table 样式美化

--- question-ms/src/main/java/com/exception/qms/utils/MarkdownUtil.java
+++ question-ms/src/main/java/com/exception/qms/utils/MarkdownUtil.java
@@ -1,25 +1,44 @@
 package com.exception.qms.utils;
 
+import com.vladsch.flexmark.Extension;
 import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.emoji.EmojiExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.profiles.pegdown.Extensions;
+import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
+import com.vladsch.flexmark.util.options.DataHolder;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
 
 /**
  * @author jiangbing(江冰)
  * @date 2018/4/6
  * @time 下午12:32
- * @discription 时间工具类
+ * @discription markdown 工具类
  **/
 @Slf4j
 public class MarkdownUtil {
 
-    private static Parser parser;
-    private static HtmlRenderer renderer;
+
+    private final static Parser parser;
+    private final static HtmlRenderer renderer;
 
     static {
+        // 定制 markdown 选项
         MutableDataSet options = new MutableDataSet();
+
+        // set optional extensions
+        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(),
+                StrikethroughExtension.create()));
+
+        // convert soft-breaks to hard breaks
+        options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
         parser = Parser.builder(options).build();
         renderer = HtmlRenderer.builder(options).build();
     }
@@ -31,12 +50,6 @@ public class MarkdownUtil {
      */
     public static String parse2Html(String mdStr) {
 
-        // uncomment to set optional extensions
-        //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
-
-        // uncomment to convert soft-breaks to hard breaks
-        //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
-
         try {
             Node document = parser.parse(mdStr);
             return renderer.render(document);
@@ -44,6 +57,15 @@ public class MarkdownUtil {
             log.error("markdonw parse to html exception: ", e);
             return null;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(parse2Html("|  fdsf |  fdsf |\n" +
+                "| ------------ | ------------ |\n" +
+                "|  fdsf |fsdf   |\n" +
+                "|fsdf   |  fdsf |\n" +
+                "\n" +
+                ":warnings:"));
     }
 
 }
