e74036b912c5d3ef85c07049a78126791bfe2ff8
weiwosuoai <weiwosuoai1991@gmail.com>
2018-07-28 10:56:25 +0800

    修改 code 颜色

--- question-ms/src/test/java/com/exception/qms/ApplicationTests.java
+++ question-ms/src/test/java/com/exception/qms/ApplicationTests.java
@@ -3,8 +3,19 @@ package com.exception.qms;
 import org.junit.runner.RunWith;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ApplicationTests {
+
+//    public static void main(String[] args) {
+//        try (FileInputStream inputStream = new FileInputStream(new File("test"))) {
+//            System.out.println(inputStream.read());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e.getMessage(), e);
+//        }
+//    }
 
 
 }
