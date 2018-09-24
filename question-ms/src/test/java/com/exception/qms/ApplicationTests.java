d8a04177f0d49fce7f10adc4303fa8920c908580
weiwosuoai <weiwosuoai1991@gmail.com>
2018-09-24 16:34:49 +0800

    部分页面体验优化

--- question-ms/src/test/java/com/exception/qms/ApplicationTests.java
+++ question-ms/src/test/java/com/exception/qms/ApplicationTests.java
@@ -6,16 +6,14 @@ import javax.swing.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ApplicationTests {
 
-//    public static void main(String[] args) {
-//        try (FileInputStream inputStream = new FileInputStream(new File("test"))) {
-//            System.out.println(inputStream.read());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//    }
+    public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap();
+        hashMap.put(1, "jiangbing");
+    }
 
 
 }
